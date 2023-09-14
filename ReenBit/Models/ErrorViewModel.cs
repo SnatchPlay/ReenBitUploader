e527ac611b1832b31597ffd930e9518a7befebce
SnatchPlay <snatch44233@gmail.com>
2023-09-14 11:37:24 +0300

    Add project files.

---
+++ ReenBit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReenBit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

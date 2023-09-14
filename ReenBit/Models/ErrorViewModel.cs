675600dd9b17d7570606d72c4f141864988f0dde
SnatchPlay <snatch44233@gmail.com>
2023-09-14 12:13:46 +0300

    delete previous project

--- ReenBit/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ReenBit.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
